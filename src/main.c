66917bc960ce9961a7bdc32b3c137694315b8f90
Ariel dos Santos de Cristo Góes <santosng456@gmail.com>
2024-12-20 15:30:48 -0300

    Adicionando o primeiro arquivo

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include "area.h"
 #include <stdio.h>
 #include "armazenamento_digital.h"
-
+#include "volume.h"
 
 int main() {
     int escolha;
@@ -32,7 +32,7 @@ int main() {
                 break;
             case 3:
                 // Função para conversão de volume
-                printf("\nFuncionalidade de volume em desenvolvimento...\n");
+                 exibirMenu();
                 break;
             case 4:
                 // Função para conversão de temperatura

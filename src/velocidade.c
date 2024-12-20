172534cc4e7a4572a3ace4162b1734ee162d0f0a
brunaalvescabral <brunaalvescabralb36@gmail.com>
2024-12-20 15:59:18 -0300

    Adicionadas funções de conversão: km/h para mph e m/s para km/h

--- src/velocidade.c
+++ src/velocidade.c
@@ -7,9 +7,21 @@ double km_horas (double m_s);
 double milhas_horas (double km_h);
 
 int main(){
-    printf("%.2f", min_segundos(5));
+    printf("m/s = %.2f", min_segundos(5));
+    printf("\nkm/h = %.2f", km_horas(10));
+    printf("\nmph = %.2f", milhas_horas(36));
 }
 double min_segundos (double km_h){
     double m_s = km_h / 3.6;
     return m_s;
+}
+
+double km_horas (double m_s){
+    double km_h = m_s * 3.6;
+    return km_h;
+}
+
+double milhas_horas (double km_h){
+    double mph = km_h * 0.621371;
+    return mph;
 }
